239b783a09363860ad37590bd6ba49f344270d12
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-08-03 17:30:32 +0200

    Change video URL to a masked link

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ for (let i = 0; i < clips.length; i++) {
             {
               name: "VOD",
               value: clips[i].video_id
-                ? `https://www.twitch.tv/videos/${clips[i].video_id}`
+                ? `(${clips[i].video_id})[https://www.twitch.tv/videos/${clips[i].video_id}]`
                 : "N/A",
               inline: true,
             },
