f40e11220d0ddd04687c87559069cc32a84067bd
Wissididom <30803034+Wissididom@users.noreply.github.com>
2024-10-12 20:52:39 +0200

    Fix migration script

--- migrate-config.js
+++ migrate-config.js
@@ -2,31 +2,6 @@ import "dotenv/config";
 import fs from "node:fs";
 import readline from "node:readline";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-let cron = null;
-
-rl.question(
-  "Please enter the cron expression you want to have it run as:",
-  (cronExpression) => {
-    cron = cronExpression;
-    rl.close();
-  },
-);
-
-const configObject = {
-  twitchLogin: process.env.BROADCASTER_LOGIN,
-  cron: cron,
-  timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-  discordWebhook: process.env.DISCORD_WEBHOOK_URL,
-  pollingInterval: process.env.POLLING_INTERVAL,
-  suppressUntitled: process.env.SUPPRESS_UNTITLED.toLowerCase() == "true",
-  showCreatedDate: process.env.SHOW_CREATED_DATE.toLowerCase() == "true",
-};
-
 function readConfigIfExists(filePath) {
   if (fs.existsSync(filePath)) {
     return JSON.parse(fs.readFileSync(filePath));
@@ -35,12 +10,39 @@ function readConfigIfExists(filePath) {
   }
 }
 
-const config = readConfigIfExists(".config.json");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-config.push(configObject);
+rl.question(
+  "Please enter the cron expression you want to have it run as:\n",
+  (cron) => {
+    rl.close();
+    runMigration(cron);
+  },
+);
 
-fs.writeFileSync(".config.json", JSON.stringify(config, null, 2));
+function runMigration(cron) {
+  const configObject = {
+    twitchLogin: process.env.BROADCASTER_LOGIN,
+    cron: cron,
+    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    discordWebhook: process.env.DISCORD_WEBHOOK_URL,
+    pollingInterval: process.env.POLLING_INTERVAL,
+    suppressUntitled: process.env.SUPPRESS_UNTITLED.toLowerCase() == "true",
+    showCreatedDate: process.env.SHOW_CREATED_DATE.toLowerCase() == "true",
+  };
 
-const newEnv = `TWITCH_CLIENT_ID=${process.env.TWITCH_CLIENT_ID ?? "[0-9a-z]"}\nTWITCH_CLIENT_SECRET=${process.env.TWITCH_CLIENT_SECRET ?? "[0-9a-z]"}`;
+  const config = readConfigIfExists(".config.json");
 
-fs.writeFileSync(".env", newEnv);
+  config.push(configObject);
+
+  fs.writeFileSync(".config.json", JSON.stringify(config, null, 2));
+  console.log(JSON.stringify(config, null, 2));
+
+  const newEnv = `TWITCH_CLIENT_ID=${process.env.TWITCH_CLIENT_ID ?? "[0-9a-z]"}\nTWITCH_CLIENT_SECRET=${process.env.TWITCH_CLIENT_SECRET ?? "[0-9a-z]"}`;
+
+  fs.writeFileSync(".env", newEnv);
+  console.log(newEnv);
+}
