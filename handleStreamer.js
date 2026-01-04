27418538e9b460e55c45f3cc94cc4214632bffb3
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-01-04 14:42:52 +0100

    Migrate away from WebhookClient (#96)

--- handleStreamer.js
+++ handleStreamer.js
@@ -1,5 +1,3 @@
-import { WebhookClient, EmbedBuilder } from "discord.js";
-
 const API_BASE_URL = "https://api.twitch.tv/helix";
 
 let tokens = {
@@ -23,28 +21,40 @@ async function getTokens() {
       method: "POST",
     },
   ).then(async (res) => {
-    let result = await res.json();
+    const result = await res.json();
     result.expires_at = new Date(Date.now() + result.expires_in * 1000);
     return result;
   });
   return tokens;
 }
 
-async function fetchTwitch(endpoint, tokens) {
+async function fetchTwitch(endpoint) {
+  if (!tokens.access_token) tokens = await getTokens();
   const res = await fetch(`${API_BASE_URL}${endpoint}`, {
     headers: {
       "Client-ID": process.env.TWITCH_CLIENT_ID,
       Authorization: `Bearer ${tokens.access_token}`,
     },
   });
-  return res.json();
+  if (res.status == 401) {
+    tokens = await getTokens();
+    return await fetchTwitch(endpoint);
+  } else if (res.status == 429) {
+    const retryAfter = parseInt(response.headers.get("Retry-After") | "0", 10);
+    if (retryAfter > 0) {
+      console.log(`Rate limit hit. Retrying after ${retryAfter}s...`);
+      await new Promise((resolve) => setTimeout(resolve, retryAfter * 1000)); // wait for the retry period
+      return await fetchTwitch(endpoint);
+    }
+  }
+  return await res.json();
 }
 
-async function fetchUsersByLogins(tokens, logins) {
+async function fetchUsersByLogins(logins) {
   if (logins.length > 100) throw new Error("Too many users");
   if (logins.length < 1) return [];
   const query = `?login=${logins.join("&login=")}`;
-  const data = await fetchTwitch(`/users${query}`, tokens);
+  const data = await fetchTwitch(`/users${query}`);
   return data.data.map(({ id, login, display_name, profile_image_url }) => ({
     id,
     login,
@@ -53,11 +63,11 @@ async function fetchUsersByLogins(tokens, logins) {
   }));
 }
 
-async function fetchUsersByIds(tokens, ids) {
+async function fetchUsersByIds(ids) {
   if (ids.length > 100) throw new Error("Too many users");
   if (ids.length < 1) return [];
   const query = `?id=${ids.join("&id=")}`;
-  const data = await fetchTwitch(`/users${query}`, tokens);
+  const data = await fetchTwitch(`/users${query}`);
   return data.data.map(({ id, login, display_name, profile_image_url }) => ({
     id,
     login,
@@ -66,19 +76,19 @@ async function fetchUsersByIds(tokens, ids) {
   }));
 }
 
-async function fetchVideosByIds(tokens, ids) {
+async function fetchVideosByIds(ids) {
   if (ids.length > 100) throw new Error("Too many videos");
   if (ids.length < 1) return [];
   const query = `?id=${ids.join("&id=")}`;
-  const data = await fetchTwitch(`/videos${query}`, tokens);
+  const data = await fetchTwitch(`/videos${query}`);
   return data.data.map(({ id, title }) => ({ id, title }));
 }
 
-async function fetchGamesByIds(tokens, ids) {
+async function fetchGamesByIds(ids) {
   if (ids.length > 100) throw new Error("Too many games");
   if (ids.length < 1) return [];
   const query = `?id=${ids.join("&id=")}`;
-  const data = await fetchTwitch(`/games${query}`, tokens);
+  const data = await fetchTwitch(`/games${query}`);
   return data.data.map(({ id, name, box_art_url }) => ({
     id,
     name,
@@ -86,22 +96,20 @@ async function fetchGamesByIds(tokens, ids) {
   }));
 }
 
-async function fetchClips(tokens, broadcasterId, date) {
+async function fetchClips(broadcasterId, date) {
   const data = await fetchTwitch(
     `/clips?broadcaster_id=${broadcasterId}&first=100&started_at=${date.toISOString()}`,
-    tokens,
   );
   return data.data;
 }
 
-function createClipEmbed(clip, games, videos) {
+function createClipEmbed(clip, games) {
   const game = games.find((x) => x.id === clip.game_id);
-  const video = videos.find((x) => x.id === clip.videos_id);
 
-  return new EmbedBuilder()
-    .setTitle(clip.title.trim())
-    .setURL(clip.url)
-    .addFields(
+  return {
+    title: clip.title.trim(),
+    url: clip.url,
+    fields: [
       { name: "Game", value: game?.name ?? "N/A", inline: true },
       { name: "Streamer", value: clip.broadcaster_name ?? "N/A", inline: true },
       { name: "Clipper", value: clip.creator_name ?? "N/A", inline: true },
@@ -143,15 +151,15 @@ function createClipEmbed(clip, games, videos) {
             : "N/A",
         inline: true,
       },
-    )
-    .setThumbnail(game?.boxart)
-    .setImage(clip.thumbnail_url);
+    ],
+    thumbnail: { url: game?.boxart },
+    image: { url: clip.thumbnail_url },
+  };
 }
 
 async function processClips(
-  tokens,
   clips,
-  webhookClient,
+  webhookUrl,
   options,
   messageMap = {},
   postedIds = [],
@@ -162,9 +170,9 @@ async function processClips(
   const videoIds = [...new Set(clips.map((c) => c.video_id).filter(Boolean))]; // Make sure there are no duplicate entries
   const gameIds = [...new Set(clips.map((c) => c.game_id).filter(Boolean))]; // Make sure there are no duplicate entries
   const [users, videos, games] = await Promise.all([
-    fetchUsersByIds(tokens, creatorIds),
-    fetchVideosByIds(tokens, videoIds),
-    fetchGamesByIds(tokens, gameIds),
+    fetchUsersByIds(creatorIds),
+    fetchVideosByIds(videoIds),
+    fetchGamesByIds(gameIds),
   ]);
   for (const clip of clips) {
     if (suppressUntitled) {
@@ -179,33 +187,32 @@ async function processClips(
     if (postedIds.includes(clip.id)) {
       // Don't post clips that were already posted. Edit them, because the Clip will get returned even if no title was set yet.
       const game = games.find((x) => x.id === clip.game_id);
-      const video = videos.find((x) => x.id === clip.videos_id);
       const existing = messageMap[clip.id];
       const embed = existing?.embeds[0];
       let anythingChanged = existing?.content !== content;
       let embedChanged = false;
       if (embed) {
-        const newEmbed = new EmbedBuilder();
+        const newEmbed = {};
         const fieldUpdates = {
           title: {
             current: embed.title,
             new: clip.title?.trim(),
-            setter: (val) => newEmbed.setTitle(val),
+            setter: (val) => (newEmbed.title = val),
           },
           url: {
             current: embed.url,
             new: clip.url,
-            setter: (val) => newEmbed.setURL(val),
+            setter: (val) => (newEmbed.url = val),
           },
           thumbnail: {
             current: embed.thumbnail,
             new: game?.boxart,
-            setter: (val) => newEmbed.setThumbnail(val),
+            setter: (val) => (newEmbed.thumbnail = { url: val }),
           },
           image: {
             current: embed.image,
             new: clip.thumbnail_url,
-            setter: (val) => newEmbed.setImage(val),
+            setter: (val) => (newEmbed.image = { url: val }),
           },
         };
         for (const key in fieldUpdates) {
@@ -243,7 +250,8 @@ async function processClips(
             anythingChanged = true;
             embedChanged = true;
           }
-          newEmbed.addFields({
+          if (!newEmbed.fields) newEmbed.fields = [];
+          newEmbed.fields.push({
             name: field.name,
             value: newValue,
             inline: true,
@@ -255,22 +263,35 @@ async function processClips(
           content,
           ...(embedChanged ? { embeds: [embed] } : {}),
         };
-        const edited = await webhookClient.editMessage(
-          existing.id,
-          editPayload,
-        );
+        const edited = await fetch(`${webhookUrl}/messages/${existing.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(editPayload),
+        }).then((res) => res.json());
         messageMap[clip.id] = edited;
       }
       continue;
     }
-    const msg = await webhookClient.send({
-      username: clip.creator_name,
-      avatarURL: users.find((u) => u.id === clip.creator_id)?.profileImageUrl,
-      content,
-      embeds: [createClipEmbed(clip, games, videos)],
+    const resp = await fetch(`${webhookUrl}?wait=true`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: clip.creator_name,
+        avatar_url: users.find((u) => u.id === clip.creator_id)
+          ?.profileImageUrl,
+        content,
+        embeds: [createClipEmbed(clip, games)],
+      }),
     });
-    postedIds.push(clip.id);
-    messageMap[clip.id] = msg;
+    if (resp.ok) {
+      const json = await resp.json();
+      postedIds.push(clip.id);
+      messageMap[clip.id] = json;
+    }
   }
   return { messageMap, postedIds };
 }
@@ -283,9 +304,7 @@ export async function handleStreamer(
   showCreatedDate,
   useService = false,
 ) {
-  const tokens = await getTokens();
-  const webhookClient = new WebhookClient({ url: webhookUrl });
-  const broadcaster = await fetchUsersByLogins(tokens, [broadcasterLogin]);
+  const broadcaster = await fetchUsersByLogins([broadcasterLogin]);
   if (!broadcaster) {
     console.error(
       `Error retrieving broadcaster info. Response from Twitch: ${JSON.stringify(
@@ -308,17 +327,14 @@ export async function handleStreamer(
     );
     setInterval(async () => {
       try {
-        const tokens = await getTokens();
-        if (tokens.expires_at < new Date()) token = await getTokens();
-        let date = new Date(Math.floor(Date.now() / 1000) * 1000 - 5 * 60000);
-        let clips = await fetchClips(tokens, broadcasterId, date);
+        const date = new Date(Math.floor(Date.now() / 1000) * 1000 - 5 * 60000);
+        const clips = await fetchClips(broadcasterId, date);
         console.log(
           `${new Date().toISOString()} - ${broadcasterDisplayName} (${broadcasterLogin}) - ${JSON.stringify(clips, null, 2)}`,
         );
         ({ messageMap, postedIds } = await processClips(
-          tokens,
           clips,
-          webhookClient,
+          webhookUrl,
           {
             suppressUntitled,
             showCreatedDate,
@@ -335,7 +351,7 @@ export async function handleStreamer(
     const pollingIntervalNumber = parseInt(
       pollingInterval.substring(0, pollingInterval.length - 1),
     );
-    let date = new Date();
+    const date = new Date();
     switch (pollingInterval.substring(pollingInterval.length - 1)) {
       case "d":
         date.setDate(date.getDate() - pollingIntervalNumber);
@@ -363,12 +379,11 @@ export async function handleStreamer(
         );
         return;
     }
-    const tokens = await getTokens();
-    const clips = await fetchClips(tokens, broadcasterId, date);
+    const clips = await fetchClips(broadcasterId, date);
     console.log(
       `${new Date().toISOString()} - ${broadcasterDisplayName} (${broadcasterLogin}) - ${JSON.stringify(clips, null, 2)}`,
     );
-    await processClips(tokens, clips, webhookClient, {
+    await processClips(clips, webhookUrl, {
       suppressUntitled,
       showCreatedDate,
     });
